664e500515739271e34439547007acae0a853fa9
acronim <marinko.miskovic.hr@gmail.com>
2020-10-08 22:46:23 +0200

    Finished

--- src/posts.js
+++ src/posts.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useMediaQuery } from '@material-ui/core';
 import {
   List,
   Datagrid,
@@ -12,37 +13,42 @@ import {
   Create,
   SelectInput,
   Filter,
+  SimpleList,
 } from "react-admin";
 
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
 
+export const PostList = (props) => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  return (
+    <List filters={<PostFilter />} {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.title}
+          secondaryText={(record) => `${record.views} views`}
+          tertiaryText={(record) =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField label="User" source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <TextField source="body" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
 
-export const PostList = (props) => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
-      <TextField source="id" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="id" />
-      <TextField source="title" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
-
-// export const PostList = (props) => (
-//     <List {...props}>
-//         <SimpleList
-//             primaryText={record => record.title}
-//             secondaryText={record => `${record.views} views`}
-//             tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
-//         />
-//     </List>
-// );
-
-// title={<PostTitle />}
 export const PostEdit = (props) => (
-  <Edit  {...props}>
+  <Edit title={<PostTitle />} {...props}>
     <SimpleForm>
       <TextInput disabled source="id" />
       <ReferenceInput source="userId" reference="users">
@@ -67,10 +73,10 @@ export const PostCreate = (props) => (
 );
 
 const PostFilter = (props) => (
-    <Filter {...props}>
-      <TextInput label="Search" source="q" alwaysOn />
-      <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
-        <SelectInput optionText="name" />
-      </ReferenceInput>
-    </Filter>
-  );
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
